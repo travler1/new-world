33fbf1bf048375572a00b20506aab66dca582423
hjini <seohyeonjin787@gmail.com>
2024-08-22 14:57:33 +0900

    start-server.sh editted

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,7 +1,7 @@
 echo "--------------- 서버 배포 시작 -----------------"
 cd ~
 cd hello-world-server
-docker pull 533267276735.dkr.ecr.ap-northeast-2.amazonaws.com/hello-world/hello-world-server:latest
+docker pull 533267276735.dkr.ecr.ap-northeast-2.amazonaws.com/hello-world-server:latest
 docker compose down || true
 docker compose up -d --build
 echo "--------------- 서버 배포 끝 -----------------"
